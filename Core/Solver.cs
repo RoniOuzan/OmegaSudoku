bfa034535987a17819926dfdf829d13d6cb38441
RoniOuzan <roniouzan@gmail.com>
2026-01-07 14:05:50 +0200

    First solver with simple backtracking

---
+++ Core/Solver.cs
@@ -0,0 +1,56 @@
+﻿namespace OmegaSudoku.Core;
+
+public class Solver
+{
+    public static bool Solve(int[,] board, int column = 0, int row = 0)
+    {
+        if (row == board.GetLength(1))
+        {
+            row = 0;
+            column++;
+        }
+
+        if (column == board.GetLength(0))
+            return true;
+
+        if (board[column, row] != 0)
+            return Solve(board, column, row + 1);
+
+        for (int i = 1; i <= board.GetLength(0); i++)
+        {
+            if (!isSafe(board, column, row, i)) 
+                continue;
+            
+            board[column, row] = i;
+            if (Solve(board, column, row + 1))
+                return true;
+            board[column, row] = 0;
+        }
+
+        return false;
+    }
+
+    public static bool isSafe(int[,] board, int column, int row, int num)
+    {
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            if (board[i, row] == num)
+                return false;
+        }
+        
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            if (board[column, i] == num)
+                return false;
+        }
+
+        int startCol = column - (column % 3);
+        int startRow = row - (row % 3);
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i + startCol, j + startRow] == num)
+                    return false;
+
+        return true;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,52 @@
-﻿namespace OmegaSudoku;
+﻿using System.Diagnostics;
+using OmegaSudoku.Core;
+
+namespace OmegaSudoku;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[,] board = 
+        {
+            {3, 0, 6, 5, 0, 8, 4, 0, 0}, 
+            {5, 2, 0, 0, 0, 0, 0, 0, 0}, 
+            {0, 8, 7, 0, 0, 0, 0, 3, 1},
+            {0, 0, 3, 0, 1, 0, 0, 8, 0}, 
+            {9, 0, 0, 8, 6, 3, 0, 0, 5}, 
+            {0, 5, 0, 0, 9, 0, 6, 0, 0},
+            {1, 3, 0, 0, 0, 0, 2, 5, 0}, 
+            {0, 0, 0, 0, 0, 0, 0, 7, 4}, 
+            {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+        
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        
+        Solver.Solve(board);
+        
+        stopwatch.Stop();
+        
+        PrintBoard(board);
+        Console.WriteLine($"\nSolved in {stopwatch.ElapsedMilliseconds} ms");
+    }
+
+    static void PrintBoard(int[,] board)
+    {
+        for (int i = 0; i < board.GetLength(0); i++) {
+            if (i > 0 && i % 3 == 0)
+            {
+                Console.WriteLine("---------+---------+---------");
+            }
+
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                if (j > 0 && j % 3 == 0)
+                {
+                    Console.Write("|");
+                }
+                Console.Write(" " + board[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
